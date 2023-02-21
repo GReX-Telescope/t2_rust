e3916710fd83bffad34085c35e092bec53b18ed1
Kiran Shila <me@kiranshila.com>
2023-02-20 16:32:21 -0800

    Test loop

--- src/main.rs
+++ src/main.rs
@@ -46,43 +46,46 @@ fn cluster_params(cands: &[Candidate]) -> Array2<f64> {
 fn main() -> anyhow::Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:12345")?;
 
-    let mut cands = Vec::new();
     let mut buf = [0; 512];
-    while cands.len() < 1024 {
-        let (n, _) = socket.recv_from(&mut buf)?;
-        let cand = Candidate::from_str(std::str::from_utf8(&buf[..n]).unwrap());
-        cands.push(cand)
-    }
-
-    // Cluster (get idxs)
-    let mut clusters: HashMap<usize, Candidate> = HashMap::new();
-    let cluster_idxs = Dbscan::params(5)
-        .tolerance(14.0)
-        .transform(&cluster_params(&cands))?;
-    for (cand_i, maybe_cluster) in cluster_idxs.iter().enumerate() {
-        let cand = cands[cand_i];
-        if let Some(cluster_i) = maybe_cluster {
-            if let Some(current_cluster_cand) = clusters.get(cluster_i) {
-                // Replace if the SNR is larger
-                if cand.snr > current_cluster_cand.snr {
-                    clusters.insert(*cluster_i, cand);
-                }
-            } else {
-                clusters.insert(*cluster_i, cand);
-            }
-        }
-    }
-    // Now flatten the candidates and remove the ones we don't care about
-
     let min_dm = 20.0;
     let max_dm = 100.0;
     let min_snr = 20.0;
 
-    let filtered: Vec<_> = clusters
-        .into_values()
-        .filter(|cand| cand.snr > min_snr)
-        .filter(|cand| cand.dm > min_dm && cand.dm < max_dm)
-        .collect();
-    dbg!(filtered);
-    Ok(())
+    loop {
+        let mut cands = Vec::new();
+        while cands.len() < 1024 {
+            let (n, _) = socket.recv_from(&mut buf)?;
+            let cand = Candidate::from_str(std::str::from_utf8(&buf[..n]).unwrap());
+            cands.push(cand)
+        }
+
+        // Cluster (get idxs)
+        let mut clusters: HashMap<usize, Candidate> = HashMap::new();
+        let cluster_idxs = Dbscan::params(5)
+            .tolerance(14.0)
+            .transform(&cluster_params(&cands))?;
+
+        for (cand_i, maybe_cluster) in cluster_idxs.iter().enumerate() {
+            let cand = cands[cand_i];
+            if let Some(cluster_i) = maybe_cluster {
+                if let Some(current_cluster_cand) = clusters.get(cluster_i) {
+                    // Replace if the SNR is larger
+                    if cand.snr > current_cluster_cand.snr {
+                        clusters.insert(*cluster_i, cand);
+                    }
+                } else {
+                    clusters.insert(*cluster_i, cand);
+                }
+            }
+        }
+        // Now flatten the candidates and remove the ones we don't care about
+
+        let filtered: Vec<_> = clusters
+            .into_values()
+            .filter(|cand| cand.snr > min_snr)
+            .filter(|cand| cand.dm > min_dm && cand.dm < max_dm)
+            .collect();
+        dbg!(filtered);
+        cands.clear();
+    }
 }
