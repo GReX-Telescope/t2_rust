7147a5150f455eeca1b63efd2a13065d3b7f3baf
Kiran Shila <me@kiranshila.com>
2024-02-03 14:38:27 -0800

    remove unsigned ibox

--- migrations/0001_t2_cands_table.sql
+++ migrations/0001_t2_cands_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE t2_cands (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     mjds REAL NOT NULL,
     snr REAL NOT NULL,
-    ibox INT UNSINGED NOT NULL,
+    ibox INT NOT NULL,
     dm REAL NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
